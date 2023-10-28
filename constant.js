ae4463dd081aaecc76599d948a52a4c0d55efc36
Ja-10 <147098779+Ja-10@users.noreply.github.com>
2023-10-28 10:48:00 +0530

    Add files via upload

---
+++ constant.js
@@ -0,0 +1,25 @@
+const prompts = [
+    ["hi", "hello", "good morning"],
+    ["how are you"],
+    ["what is going on", "what is up"],
+    ["what is your name"],
+    ["bye"]
+  ]
+
+  const replies = [
+    ["Hello!", "Hey!", "Hi there!"],
+    [
+      "Fine!!what about you?",
+      "Great!!How are you?"
+    ],
+    [
+      "Nothing much",
+    ],
+    ["I am MieBot"],
+    ["Goodbye"]
+  ]
+  
+  const alternative = [
+    "Try again",
+  ]
+  
